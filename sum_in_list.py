e5dbd9c0ebd436e6879d89a3b91353b5bac51c80
SydneyGift <sydgift01@gmail.com>
2025-05-28 21:43:29 -0400

    Added sum_in_list.py to find sum of things within a list

---
+++ sum_in_list.py
@@ -0,0 +1,11 @@
+#Calculate sum of numbers in a list
+
+numbers = [1, 5, 3, 9]
+total = 0
+
+#Iterate through the list using a for loop and add the number to the variable, total.
+
+for number in numbers:
+    total = total + number
+
+print("The total of the numbers is", total)
