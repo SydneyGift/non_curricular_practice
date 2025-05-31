1ba9a56ecbd9e0ae86535f1826cb2b9ed91e6e54
SydneyGift <sydgift01@gmail.com>
2025-05-31 10:29:43 -0400

    Added comments on  pattern_print.py to explain logic behind each action

--- pattern_print.py
+++ pattern_print.py
@@ -2,9 +2,9 @@
 
 size = int(input("Enter the size of the pattern between 1 and 10: "))
 
-while 1 <= size <= 10:
-    for rows in range(1, size+1, 1):
-        for columns in range(1, rows+1, 1):
+while 1 <= size <= 10:#Make sure the pattern can only be between one and ten
+    for rows in range(1, size+1, 1):#This will execute size+1 times thus giving the total number of size rows
+        for columns in range(1, rows+1, 1):#This will execute rows' time for every outer loop execution
             print(columns, end="")
-        print("")
-    size -= size
+        print("")#After printing each row, go to the next line i.e empty line after each row.
+    size -= size#So that the while loop only runs once.
